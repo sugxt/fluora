693590466955ab7e3d75640c918b55f92e5893a2
Sugat Poudyal <official.sugat@gmail.com>
2025-04-28 22:00:42 +0545

    Changed Import Style

--- src/scene/titleText.js
+++ src/scene/titleText.js
@@ -2,24 +2,26 @@ import * as THREE from "three";
 import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
 import { scene } from "./createScene.js";
 import { FontLoader } from "three/examples/jsm/Addons.js";
+import fontJSON from "../assets/fonts/BoldFloral.json";  // ✅ Correct import
 
 const loader = new FontLoader();
 
-loader.load("/src/assets/fonts/BoldFloral.json", function (font) {
-  const textGeometry = new TextGeometry("Fluora", {
-    font: font,
-    size: 1,
-    depth: 0.2,
-  });
+// 👉 Instead of loader.load(), directly parse the imported font JSON
+const font = loader.parse(fontJSON);
 
-  const textMaterial = new THREE.MeshStandardMaterial({
-    color: 0xffc0cb,
-    emissive: 0xffc0cb,
-    emissiveIntensity: 0.5,
-  });
-
-  const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-
-  textMesh.position.set(-2.4, 0, -7);
-  scene.add(textMesh);
+const textGeometry = new TextGeometry("Fluora", {
+  font: font,
+  size: 1,
+  depth: 0.2,
 });
+
+const textMaterial = new THREE.MeshStandardMaterial({
+  color: 0xffc0cb,
+  emissive: 0xffc0cb,
+  emissiveIntensity: 0.5,
+});
+
+const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+
+textMesh.position.set(-2.4, 0, -7);
+scene.add(textMesh);
